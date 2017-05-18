e7a896f7893095a58776b8f21e08ec878d4c18e8
Stephen Sprinkle <stephen@sprinkle.io>
2017-05-18 09:38:30 -0600

    fix to es6 transpilation to es5

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,5 @@
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 import React, { Component, PropTypes } from 'react';
 
 export default class Identicon extends Component {
@@ -12,7 +14,7 @@ export default class Identicon extends Component {
   }
 
   componentWillUpdate(nextProps) {
-    if (!isEquivalent(this.props, nextProps)) this.generateIdenticon({ ...nextProps });
+    if (!isEquivalent(this.props, nextProps)) this.generateIdenticon(_extends({}, nextProps));
   }
 
   generateIdenticon(options) {
@@ -27,29 +29,29 @@ export default class Identicon extends Component {
         randseed[i] = 0;
       }
       for (let i = 0; i < seed.length; i++) {
-        randseed[i%4] = ((randseed[i%4] << 5) - randseed[i%4]) + seed.charCodeAt(i);
+        randseed[i % 4] = (randseed[i % 4] << 5) - randseed[i % 4] + seed.charCodeAt(i);
       }
     }
 
     function rand() {
       // based on Java's String.hashCode(), expanded to 4 32bit values
-      const t = randseed[0] ^ (randseed[0] << 11);
+      const t = randseed[0] ^ randseed[0] << 11;
 
       randseed[0] = randseed[1];
       randseed[1] = randseed[2];
       randseed[2] = randseed[3];
-      randseed[3] = (randseed[3] ^ (randseed[3] >> 19) ^ t ^ (t >> 8));
+      randseed[3] = randseed[3] ^ randseed[3] >> 19 ^ t ^ t >> 8;
 
-      return (randseed[3]>>>0) / ((1 << 31)>>>0);
+      return (randseed[3] >>> 0) / (1 << 31 >>> 0);
     }
 
     function createColor() {
       // saturation is the whole color spectrum
       const h = Math.floor(rand() * 360);
       // saturation goes from 40 to 100, it avoids greyish colors
-      const s = ((rand() * 60) + 40) + '%';
+      const s = rand() * 60 + 40 + '%';
       // lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
-      const l = ((rand()+rand()+rand()+rand()) * 25) + '%';
+      const l = (rand() + rand() + rand() + rand()) * 25 + '%';
 
       const color = 'hsl(' + h + ',' + s + ',' + l + ')';
       return color;
@@ -68,7 +70,7 @@ export default class Identicon extends Component {
         for (let x = 0; x < dataWidth; x++) {
           // this makes foreground and background color to have a 43% (1/2.3) probability
           // spot color has 13% chance
-          row[x] = Math.floor(rand()*2.3);
+          row[x] = Math.floor(rand() * 2.3);
         }
         const r = row.slice(0, mirrorWidth);
         r.reverse();
@@ -94,7 +96,7 @@ export default class Identicon extends Component {
 
       for (let i = 0; i < imageData.length; i++) {
         // if data is 2, choose spot color, if 1 choose foreground
-        cc.fillStyle = (imageData[i] === 1) ? color : spotcolor;
+        cc.fillStyle = imageData[i] === 1 ? color : spotcolor;
 
         // if data is 0, leave the background
         if (imageData[i]) {
@@ -109,7 +111,7 @@ export default class Identicon extends Component {
     const opts = options || {};
     const size = opts.size || 8;
     const scale = opts.scale || 4;
-    const seed = opts.seed || Math.floor((Math.random()*Math.pow(10,16))).toString(16);
+    const seed = opts.seed || Math.floor(Math.random() * Math.pow(10, 16)).toString(16);
 
     seedrand(seed);
 
@@ -123,9 +125,9 @@ export default class Identicon extends Component {
   }
 
   render() {
-    return (
-      React.createElement("canvas", {ref: (identicon) => { this.identicon = identicon; }})
-    );
+    return React.createElement('canvas', { ref: identicon => {
+        this.identicon = identicon;
+      } });
   }
 }
 
@@ -136,7 +138,7 @@ Identicon.propTypes = {
   color: PropTypes.string,
   bgColor: PropTypes.string,
   spotColor: PropTypes.string
-}
+};
 
 function isEquivalent(prevProps, nextProps) {
   var aProps = Object.getOwnPropertyNames(prevProps);
@@ -150,7 +152,7 @@ function isEquivalent(prevProps, nextProps) {
     var propName = aProps[i];
 
     if (prevProps[propName] !== nextProps[propName]) {
-        return false;
+      return false;
     }
   }
 
