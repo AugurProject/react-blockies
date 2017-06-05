d758b54247dd7b386fbc0769997489600b56b99d
Alex Goldvarg <agoldvarg@gmail.com>
2017-06-05 14:24:03 -0400

    fix warning

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Identicon extends Component {
   constructor(props) {
